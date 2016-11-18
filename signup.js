7998c007c4baf78f0f5ef9d5feef1583b857a8d6
JackBrasesco <jacbras@nuevaschool.org>
2016-11-18 09:04:09 -0800

    Classadoo autocommit

--- signup.js
+++ signup.js
@@ -1,7 +1,8 @@
-var newUser = inputUser.val();
-var newPass1 = $("#inputPass1").val();
-var newPass2 = $("#inputPass2").val();
-obj = {
-  name:
-  prof
-}
+//var newUser = inputUser.val();
+//var newPass1 = $("#inputPass1").val();
+//var newPass2 = $("#inputPass2").val();
+
+
+//localStorage["name"] = "david"
+
+console.log(localStorage["name"])
