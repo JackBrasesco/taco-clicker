522d7e1b2beb2b7a89e93890ce62b0ab6742a41a
JackBrasesco <jacbras@nuevaschool.org>
2016-11-30 09:18:12 -0800

    Classadoo autocommit

--- upgrade.js
+++ upgrade.js
@@ -14,13 +14,14 @@ $("#item1").click(function() {
   localStorage.setItem("is2x",1)
   clickAdd = (clickAdd * 2);
   localStorage.setItem("b",clickAdd)
+  localStorage.setItem("c",price1)
   price1 = (price1 * 3);
   $("#price1").html("$" + price1)
   }
   else if (tacoCount < price1) {
-    $("#text1").html("you cannot afford this item.")
+    $("#text1").html("you cannot afford this item.");
     setTimeout(function() {
-      $("#text1").html("Multiply your <br>
+      $("#text1").html("Multiply your <br> click by 2!");
     }, 2000)
   }
 })
