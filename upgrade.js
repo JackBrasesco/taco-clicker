62c648531f092d9f737f02cece1398c52fa2f3e7
JackBrasesco <jacbras@nuevaschool.org>
2016-12-01 15:01:00 -0800

    Classadoo autocommit

--- upgrade.js
+++ upgrade.js
@@ -29,6 +29,7 @@ else {
 $("#item2").click(function() {
   animateObject2()
   if (tacoCount >= price2) {
+    idleAdd = (idleAdd + 10);
     localStorage.setItem("isTacoBell", 1)
     price2 = (price2 * 1.57);
     localStorage.setItem("d", price2)
